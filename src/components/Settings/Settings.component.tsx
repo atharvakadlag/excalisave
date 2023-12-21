2adc5f64f34f56a0e24694dd247fbb8c26f8521c
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-12-21 00:29:34 -0500

    Export files as json

--- src/components/Settings/Settings.component.tsx
+++ src/components/Settings/Settings.component.tsx
@@ -4,39 +4,68 @@ import {
   InfoCircledIcon,
 } from "@radix-ui/react-icons";
 import { Box, Button, Callout, Flex, Text } from "@radix-ui/themes";
-import React from "react";
+import React, { useEffect } from "react";
 import { browser } from "webextension-polyfill-ts";
 import { IDrawing } from "../../interfaces/drawing.interface";
 import JSZip from "jszip";
 import FileSaver from "file-saver";
+import { TabUtils } from "../../lib/utils/tab.utils";
+import { XLogger } from "../../lib/logger";
+import { ExportStore, MessageType } from "../../constants/message.types";
 
 const CalloutText = Callout.Text as any;
 
 export function Settings() {
   const onExportClick = async () => {
-    const result = await browser.storage.local.get();
+    const activeTab = await TabUtils.getActiveTab();
 
-    const drawings: IDrawing[] = [];
-    const favories: string[] = result["favorites"] || [];
+    if (!activeTab) {
+      XLogger.warn("No active tab found");
 
-    Object.entries(result).forEach(([key, value]) => {
-      if (key.startsWith("drawing")) {
-        drawings.push(value);
-      }
-    });
+      return;
+    }
 
-    const fileBlob = new Blob([JSON.stringify({ drawings, favories })], {
-      type: "application/json",
-    });
-
-    const zipFile = new JSZip();
-
-    zipFile.file("data.json", fileBlob);
-
-    zipFile.generateAsync({ type: "blob" }).then((content) => {
-      FileSaver.saveAs(content, `drawings-${Date.now()}.zip`);
+    await browser.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      files: ["./js/execute-scripts/export-store.bundle.js"],
     });
   };
+
+  useEffect(() => {
+    browser.runtime.onMessage.addListener(async (message: ExportStore) => {
+      if (message.type === MessageType.EXPORT_STORE) {
+        const result = await browser.storage.local.get();
+
+        const drawings: IDrawing[] = [];
+        const favories: string[] = result["favorites"] || [];
+
+        Object.entries(result).forEach(([key, value]) => {
+          if (key.startsWith("drawing")) {
+            drawings.push(value);
+          }
+        });
+
+        const fileBlob = new Blob([JSON.stringify({ drawings, favories })], {
+          type: "application/json",
+        });
+
+        const zipFile = new JSZip();
+
+        zipFile.file("data.json", fileBlob);
+
+        message.payload.files?.forEach((file) => {
+          zipFile
+            .folder("files-store")
+            .file(`${file.id}.json`, JSON.stringify(file));
+        });
+
+        // Save file
+        zipFile.generateAsync({ type: "blob" }).then((content) => {
+          FileSaver.saveAs(content, `drawings-${Date.now()}.zip`);
+        });
+      }
+    });
+  }, []);
 
   return (
     <Flex direction="column" p={"2"} gap={"3"}>
