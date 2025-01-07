5994fd073a11839c62cd2b23da620cd3cb741598
Sai Teja Madha <git@saitejamadha.dev>
2025-01-07 16:24:43 -0700

    match upstream formatting

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,167 +1,163 @@
-import { browser } from 'webextension-polyfill-ts';
+import { browser } from "webextension-polyfill-ts";
 import {
-	CleanupFilesMessage,
-	MessageType,
-	SaveDrawingMessage,
-	SaveNewDrawingMessage,
-} from '../constants/message.types';
-import { IDrawing } from '../interfaces/drawing.interface';
-import { XLogger } from '../lib/logger';
-import { TabUtils } from '../lib/utils/tab.utils';
-import { RandomUtils } from '../lib/utils/random.utils';
-import { DrawingStore } from '../lib/drawing-store';
-import { useCurrentDrawingId } from '../Popup/hooks/useCurrentDrawing.hook';
+  CleanupFilesMessage,
+  MessageType,
+  SaveDrawingMessage,
+  SaveNewDrawingMessage,
+} from "../constants/message.types";
+import { IDrawing } from "../interfaces/drawing.interface";
+import { XLogger } from "../lib/logger";
+import { TabUtils } from "../lib/utils/tab.utils";
+import { RandomUtils } from "../lib/utils/random.utils";
+import { DrawingStore } from "../lib/drawing-store";
+import { useCurrentDrawingId } from "../Popup/hooks/useCurrentDrawing.hook";
 
 browser.runtime.onInstalled.addListener(async () => {
-	XLogger.log('onInstalled...');
+  XLogger.log("onInstalled...");
 
-	for (const cs of (browser.runtime.getManifest() as any).content_scripts) {
-		for (const tab of await browser.tabs.query({ url: cs.matches })) {
-			browser.scripting.executeScript({
-				target: { tabId: tab.id },
-				files: cs.js,
-			});
-		}
-	}
+  for (const cs of (browser.runtime.getManifest() as any).content_scripts) {
+    for (const tab of await browser.tabs.query({ url: cs.matches })) {
+      browser.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: cs.js,
+      });
+    }
+  }
 });
 
 browser.runtime.onMessage.addListener(
-	async (
-		message:
-			| SaveDrawingMessage
-			| SaveNewDrawingMessage
-			| CleanupFilesMessage
-			| any,
-		_sender: any
-	) => {
-		try {
-			XLogger.log('Mesage brackground', message);
-			if (!message || !message.type) return;
+  async (
+    message: SaveDrawingMessage | SaveNewDrawingMessage | CleanupFilesMessage | any,
+    _sender: any
+  ) => {
+    try {
+      XLogger.log("Mesage brackground", message);
+      if (!message || !message.type) return;
 
-			switch (message.type) {
-				case 'OpenPopup':
-					browser.action.openPopup();
-					break;
+      switch (message.type) {
+        case "OpenPopup":
+          browser.action.openPopup();
+          break;
 
-				case MessageType.SAVE_NEW_DRAWING:
-					await browser.storage.local.set({
-						[message.payload.id]: {
-							id: message.payload.id,
-							name: message.payload.name,
-							createdAt: new Date().toISOString(),
-							imageBase64: message.payload.imageBase64,
-							viewBackgroundColor: message.payload.viewBackgroundColor,
-							data: {
-								excalidraw: message.payload.excalidraw,
-								excalidrawState: message.payload.excalidrawState,
-								versionFiles: message.payload.versionFiles,
-								versionDataState: message.payload.versionDataState,
-							},
-						},
-					});
-					break;
+        case MessageType.SAVE_NEW_DRAWING:
+          await browser.storage.local.set({
+            [message.payload.id]: {
+              id: message.payload.id,
+              name: message.payload.name,
+              createdAt: new Date().toISOString(),
+              imageBase64: message.payload.imageBase64,
+              viewBackgroundColor: message.payload.viewBackgroundColor,
+              data: {
+                excalidraw: message.payload.excalidraw,
+                excalidrawState: message.payload.excalidrawState,
+                versionFiles: message.payload.versionFiles,
+                versionDataState: message.payload.versionDataState,
+              },
+            },
+          });
+          break;
 
-				case MessageType.SAVE_DRAWING:
-					const exitentDrawing = (
-						await browser.storage.local.get(message.payload.id)
-					)[message.payload.id] as IDrawing;
+        case MessageType.SAVE_DRAWING:
+          const exitentDrawing = (
+            await browser.storage.local.get(message.payload.id)
+          )[message.payload.id] as IDrawing;
 
-					if (!exitentDrawing) {
-						XLogger.error('No drawing found with id', message.payload.id);
-						return;
-					}
+          if (!exitentDrawing) {
+            XLogger.error("No drawing found with id", message.payload.id);
+            return;
+          }
 
-					const newData: IDrawing = {
-						...exitentDrawing,
-						name: message.payload.name || exitentDrawing.name,
-						imageBase64:
-							message.payload.imageBase64 || exitentDrawing.imageBase64,
-						viewBackgroundColor:
-							message.payload.viewBackgroundColor ||
-							exitentDrawing.viewBackgroundColor,
-						data: {
-							excalidraw: message.payload.excalidraw,
-							excalidrawState: message.payload.excalidrawState,
-							versionFiles: message.payload.versionFiles,
-							versionDataState: message.payload.versionDataState,
-						},
-					};
+          const newData: IDrawing = {
+            ...exitentDrawing,
+            name: message.payload.name || exitentDrawing.name,
+            imageBase64:
+              message.payload.imageBase64 || exitentDrawing.imageBase64,
+            viewBackgroundColor:
+              message.payload.viewBackgroundColor ||
+              exitentDrawing.viewBackgroundColor,
+            data: {
+              excalidraw: message.payload.excalidraw,
+              excalidrawState: message.payload.excalidrawState,
+              versionFiles: message.payload.versionFiles,
+              versionDataState: message.payload.versionDataState,
+            },
+          };
 
-					await browser.storage.local.set({
-						[message.payload.id]: newData,
-					});
-					break;
+          await browser.storage.local.set({
+            [message.payload.id]: newData,
+          });
+          break;
 
-				case MessageType.CLEANUP_FILES:
-					XLogger.info('Cleaning up files');
+        case MessageType.CLEANUP_FILES:
+          XLogger.info("Cleaning up files");
 
-					const drawings = Object.values(
-						await browser.storage.local.get()
-					).filter((o) => o?.id?.startsWith?.('drawing:'));
+          const drawings = Object.values(
+            await browser.storage.local.get()
+          ).filter((o) => o?.id?.startsWith?.("drawing:"));
 
-					const imagesUsed = drawings
-						.map((drawing) => {
-							return JSON.parse(drawing.data.excalidraw).filter(
-								(item: any) => item.type === 'image'
-							);
-						})
-						.flat()
-						.map<string>((item) => item?.fileId);
+          const imagesUsed = drawings
+            .map((drawing) => {
+              return JSON.parse(drawing.data.excalidraw).filter(
+                (item: any) => item.type === "image"
+              );
+            })
+            .flat()
+            .map<string>((item) => item?.fileId);
 
-					const uniqueImagesUsed = Array.from(new Set(imagesUsed));
+          const uniqueImagesUsed = Array.from(new Set(imagesUsed));
 
-					XLogger.log('Used fileIds', uniqueImagesUsed);
+          XLogger.log("Used fileIds", uniqueImagesUsed);
 
-					// This workaround is to pass params to script, it's ugly but it works
-					await browser.scripting.executeScript({
-						target: {
-							tabId: message.payload.tabId,
-						},
-						func: (fileIds: string[], executionTimestamp: number) => {
-							window.__SCRIPT_PARAMS__ = { fileIds, executionTimestamp };
-						},
-						args: [uniqueImagesUsed, message.payload.executionTimestamp],
-					});
+          // This workaround is to pass params to script, it's ugly but it works
+          await browser.scripting.executeScript({
+            target: {
+              tabId: message.payload.tabId,
+            },
+            func: (fileIds: string[], executionTimestamp: number) => {
+              window.__SCRIPT_PARAMS__ = { fileIds, executionTimestamp };
+            },
+            args: [uniqueImagesUsed, message.payload.executionTimestamp],
+          });
 
-					await browser.scripting.executeScript({
-						target: { tabId: message.payload.tabId },
-						files: ['./js/execute-scripts/delete-unused-files.bundle.js'],
-					});
+          await browser.scripting.executeScript({
+            target: { tabId: message.payload.tabId },
+            files: ["./js/execute-scripts/delete-unused-files.bundle.js"],
+          });
 
-					break;
+          break;
 
-				case 'MessageAutoSave':
-					const name = message.payload.name;
-					const setCurrent = message.payload.setCurrent;
-					XLogger.log('Saving new drawing', { name });
-					const activeTab = await TabUtils.getActiveTab();
+        case "MessageAutoSave":
+          const name = message.payload.name;
+          const setCurrent = message.payload.setCurrent;
+          XLogger.log("Saving new drawing", { name });
+          const activeTab = await TabUtils.getActiveTab();
 
-					if (!activeTab) {
-						XLogger.warn('No active tab found');
-						return;
-					}
+          if (!activeTab) {
+            XLogger.warn("No active tab found");
+            return;
+          }
 
-					const id = `drawing:${RandomUtils.generateRandomId()}`;
+          const id = `drawing:${RandomUtils.generateRandomId()}`;
 
-					// This workaround is to pass params to script, it's ugly but it works
-					await browser.scripting.executeScript({
-						target: { tabId: activeTab.id },
-						func: (id, name, setCurrent) => {
-							window.__SCRIPT_PARAMS__ = { id, name, setCurrent };
-						},
-						args: [id, name, setCurrent],
-					});
+          // This workaround is to pass params to script, it's ugly but it works
+          await browser.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            func: (id, name, setCurrent) => {
+              window.__SCRIPT_PARAMS__ = { id, name, setCurrent };
+            },
+            args: [id, name, setCurrent],
+          });
 
-					await browser.scripting.executeScript({
-						target: { tabId: activeTab.id },
-						files: ['./js/execute-scripts/sendDrawingDataToSave.bundle.js'],
-					});
-					break;
-				default:
-					break;
-			}
-		} catch (error) {
-			XLogger.error('Error on background message listener', error);
-		}
-	}
+          await browser.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            files: ["./js/execute-scripts/sendDrawingDataToSave.bundle.js"],
+          });
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      XLogger.error("Error on background message listener", error);
+    }
+  }
 );
